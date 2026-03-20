b186740627f8e7cfbf72b85898c7f2bc2ab366c7
optic-fiber <403username@gmx.net>
2026-03-20 19:24:27 +0100

    Update main.py
    A small improvement but it sill can only grab banner from ssh ports but now can filter between closed and open ports and has a scan funktion

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
+#!/usr/bin/python
+
 import socket
 
-print("#"*20)
-print("Banner grabber")
-print("#"*20)
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-def banner(ip, port):
-    try:
-        s = socket.socket()
-        s.connect((ip, int(port)))
-        s.settimeout(5)
-        data = s.recv(1048576)
-        print(data)
-        s.close()
-    except:
-        print("connection refused")
+
+print("#"*30)
+print("     Simple port scanner     ")
+print("#"*30)
+print("\n")
 
 def main():
-    ip = input("Please enter the IP: ")
-    port = input("Please enter the Port: ")
-    banner(ip, port)
+    def portscanner():
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        if s.connect_ex((host,port)):
+            print(f"Port {port} is closed!")
+        else:
+            print(f"Port {port} is open!")
+            try:
+                s = socket.socket()
+                s.connect((host, int(port)))
+                s.settimeout(5)
+                data = s.recv(1048576)
+                print(data)
+                s.close()
+            except:
+                print("connection refused")
+        s.close()
 
-main() 
+    host =str(input("Enter the Host: "))
+    what =int(input("Scan one port(1) or scan a range(2): "))
+
+    if what == 1:
+        port =int(input("Enter the Port: "))
+        portscanner()
+    elif what == 2:
+        portrangee = int(input("Enter the start port: "))
+        portrangez = int(input("Enter the end port: "))
+        for port in range(portrangee, portrangez + 1):
+            portscanner()
+
+    else:
+	print("only one and tow allowed!")
+        main()
+
+main()
